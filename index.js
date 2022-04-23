73e5c078cf58cb8fcfc14975dbd75cf052707fa2
Blaise Irakoze <birakoze@qtsoftwareltd.com>
2022-04-23 23:37:42 +0200

    refactor code

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// import express from "express";
-// import ldap from "ldapjs";
 const express = require("express");
 const ldap = require("ldapjs");
 
@@ -8,44 +6,49 @@ app.listen(3000, function () {
     console.log("server is started");
 });
 
+const client = ldap.createClient({
+    url: ["ldap://127.0.0.1:10389"],
+});
+
+//authantication
 function authentication(username, password) {
-    const client = ldap.createClient({
-        url: ["ldap://127.0.0.1:10389"],
-    });
     client.bind(username, password, (err) => {
         if (err) {
             console.log("error: ", err);
         } else {
             console.log("connection success");
+        }
+    });
+}
+authentication("uid=admin,ou=system", "secret");
 
-            const opts = {
-                filter: "(objectClass=*)",
-                scope: "sub",
-                attributes: ["sn", "cn"],
-            };
-            client.search("ou=users,ou=system", opts, (err, res) => {
-                if (err) {
-                    console.log("error searching------------------ ", err);
-                } else {
-                    res.on("searchRequest", (searchRequest) => {
-                        console.log("searchRequest: ", searchRequest.messageID);
-                    });
-                    res.on("searchEntry", (entry) => {
-                        console.log("entry: " + JSON.stringify(entry.object));
-                    });
-                    res.on("searchReference", (referral) => {
-                        console.log("referral: " + referral.uris.join());
-                    });
-                    res.on("error", (err) => {
-                        console.error("error: " + err.message);
-                    });
-                    res.on("end", (result) => {
-                        console.log("status: " + result.status);
-                    });
-                }
+// search a user
+function search() {
+    const opts = {
+        filter: "(objectClass=*)",
+        scope: "sub",
+        attributes: ["sn", "cn"],
+    };
+    client.search("ou=users,ou=system", opts, (err, res) => {
+        if (err) {
+            console.log("error searching------------------ ", err);
+        } else {
+            res.on("searchRequest", (searchRequest) => {
+                console.log("searchRequest: ", searchRequest.messageID);
+            });
+            res.on("searchEntry", (entry) => {
+                console.log("entry: " + JSON.stringify(entry.object));
+            });
+            res.on("searchReference", (referral) => {
+                console.log("referral: " + referral.uris.join());
+            });
+            res.on("error", (err) => {
+                console.error("error: " + err.message);
+            });
+            res.on("end", (result) => {
+                console.log("status: " + result.status);
             });
         }
     });
 }
-
-authentication("uid=admin,ou=system", "secret");
+search();
